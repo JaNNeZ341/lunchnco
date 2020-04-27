4b332e2e44ff83090b51b667c42cd4a711f54ef3
arvy <thotdoedoe@gmail.com>
2020-04-28 01:37:15 +0700

    Update 1_depan.blade.php
    4/28/20 (1:36)

--- resources/views/1_depan.blade.php
+++ resources/views/1_depan.blade.php
@@ -3,8 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-        <title>[GANTI YA!]</title>
+        <title>Lunch&CO</title>
 
         <!-- Styles -->
         <style>
@@ -12,6 +15,71 @@
         </style>
     </head>
     <body>
+    <div class="container-sm">
+    
+        <nav class="navbar navbar-light bg-light" style=background-color: #ff6347;>
+        <a class="navbar-brand" href="#">
+        <img src="https://imgur.com/aE4Kn1B"  width="30" height="30" class="d-inline-block align-top" alt="">LUNCH&CO
+        </a>
+        </nav>
+
+
+
+
+        <div class="jumbotron jumbotron-fluid text-center" style="background-color: aqua;">
+            <h1 class="display-1">CV</h1>
+            <p class="lead">05211840000114</p>
+            <p class="lead">Kota Padang</p>
+            <p class="lead">Arvy Muhammad Reyhan</p>
+            <p class="lead">082337748598</p>
+
+        </div>
+
+        <div class="row">
+            <div class="col-sm-4">
+                <h2 >Name :</h2>
+            </div>            
+            <div class="col-sm-8">
+                <p class="d-block p-2 bg-primary text-white">Arvy Muhammad Reyhan</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-4">
+                <h2>Tempat/Tanggal lahir :</h2>
+            </div>            
+            <div class="col-sm-8">
+                <p class="d-block p-2 bg-primary text-white">Padang/27-07-2000</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-4">
+                <h2>Riwayat Pendidika :</h2>
+            </div>
+            <div class="col-sm-8">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">(2015-2018) SMA IC ALKAUSAR</li>
+                    <li class="list-group-item">(2019-now) Institut teknologi sepuluh nopember</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-4">
+                <h2>Skill :</h2>
+            </div>
+            <div class="col-sm-8">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">1. Excelent at english</li>
+                    <li class="list-group-item">2. Good in Speaking</li>
+                    <li class="list-group-item">3. Can understand English</li>
+                    <li class="list-group-item">4. Understand UX</li>
+                    <li class="list-group-item">5. A team player</li>
+                </ul>
+            </div>
+        </div>
+
         
     </body>
 </html>
